a62c8d78e2acdb9519dac8ba6741d51d2459b5f9
Alex Grimes <alex@alexgrimes.com>
2020-11-09 07:56:41 -0600

    first order low pass

---
+++ sample-player-firstOrderLowPass/render.cpp
@@ -0,0 +1,64 @@
+/*
+ ____  _____ _        _    
+| __ )| ____| |      / \   
+|  _ \|  _| | |     / _ \  
+| |_) | |___| |___ / ___ \ 
+|____/|_____|_____/_/   \_\
+
+http://bela.io
+
+C++ Real-Time Audio Programming with Bela - Lecture 8: Filters
+sample-player-filter: template code for implementing digital filters
+*/
+
+#include <Bela.h>
+#include "MonoFilePlayer.h"
+
+// Name of the sound file (in project folder)
+std::string gFilename = "slow-drum-loop.wav";
+
+// Object that handles playing sound from a buffer
+MonoFilePlayer gPlayer;
+
+// TODO: declare global variable(s) to keep track of filter state
+// this holds x[n-1]
+
+float gLastOutput = 0;
+float gAlpha = 0.99;
+
+bool setup(BelaContext *context, void *userData)
+{
+	// Load the audio file
+	if(!gPlayer.setup(gFilename)) {
+    	rt_printf("Error loading audio file '%s'\n", gFilename.c_str());
+    	return false;
+	}
+
+	// Print some useful info
+    rt_printf("Loaded the audio file '%s' with %d frames (%.1f seconds)\n", 
+    			gFilename.c_str(), gPlayer.size(),
+    			gPlayer.size() / context->audioSampleRate);
+
+	return true;
+}
+
+void render(BelaContext *context, void *userData)
+{
+    for(unsigned int n = 0; n < context->audioFrames; n++) {
+        float in = gPlayer.process();
+
+        // y[n] = alpha * y[n-1] + 1 - alpha) * x[n]
+        float out = gAlpha * gLastOutput + (1.0 - gAlpha) * in;
+        gLastOutput = out;
+
+    	for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
+			// Write the sample to every audio output channel
+    		audioWrite(context, n, channel, out);
+    	}
+    }
+}
+
+void cleanup(BelaContext *context, void *userData)
+{
+
+}
