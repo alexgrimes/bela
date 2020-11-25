40ce7dcb76f9e16335da420360c368ffa878d9c3
Alex Grimes <alex@alexgrimes.com>
2020-11-25 17:08:09 -0600

    multithreaded fft

---
+++ fft-overlap-add-threads/build/MonoFilePlayer.d
@@ -0,0 +1,8 @@
+/root/Bela/projects/fft-overlap-add-threads/build/MonoFilePlayer.o: \
+  /root/Bela/projects/fft-overlap-add-threads/MonoFilePlayer.cpp \
+  libraries/AudioFile/AudioFile.h \
+  /root/Bela/projects/fft-overlap-add-threads/MonoFilePlayer.h
+
+libraries/AudioFile/AudioFile.h:
+
+/root/Bela/projects/fft-overlap-add-threads/MonoFilePlayer.h:
---
+++ fft-overlap-add-threads/build/render.d
@@ -0,0 +1,40 @@
+/root/Bela/projects/fft-overlap-add-threads/build/render.o: \
+  /root/Bela/projects/fft-overlap-add-threads/render.cpp include/Bela.h \
+  /usr/xenomai/include/cobalt/unistd.h \
+  /usr/xenomai/include/cobalt/stdio.h include/GPIOcontrol.h \
+  include/Utilities.h libraries/Fft/Fft.h libraries/ne10/NE10.h \
+  libraries/math_neon/math_neon.h libraries/Scope/Scope.h \
+  include/AuxTaskNonRT.h /usr/xenomai/include/cobalt/pthread.h \
+  include/AuxTaskRT.h /usr/xenomai/include/cobalt/fcntl.h \
+  /usr/xenomai/include/cobalt/mqueue.h \
+  /root/Bela/projects/fft-overlap-add-threads/MonoFilePlayer.h
+
+include/Bela.h:
+
+/usr/xenomai/include/cobalt/unistd.h:
+
+/usr/xenomai/include/cobalt/stdio.h:
+
+include/GPIOcontrol.h:
+
+include/Utilities.h:
+
+libraries/Fft/Fft.h:
+
+libraries/ne10/NE10.h:
+
+libraries/math_neon/math_neon.h:
+
+libraries/Scope/Scope.h:
+
+include/AuxTaskNonRT.h:
+
+/usr/xenomai/include/cobalt/pthread.h:
+
+include/AuxTaskRT.h:
+
+/usr/xenomai/include/cobalt/fcntl.h:
+
+/usr/xenomai/include/cobalt/mqueue.h:
+
+/root/Bela/projects/fft-overlap-add-threads/MonoFilePlayer.h:
